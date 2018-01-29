efe90cbee4aaf113dbb02219f75d61fa642f9168
kuer137 <873791976@qq.com>
2018-01-29 16:06:08 +0800

    begin

---
+++ src/AppBundle/AppBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class AppBundle extends Bundle
+{
+}
